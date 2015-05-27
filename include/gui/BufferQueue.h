141642728923eafaf8651ce835add20c8379a015
fire855 <thefire855@gmail.com>
2015-05-27 21:34:40 +0200

    fix revert conflict

--- include/gui/BufferQueue.h
+++ include/gui/BufferQueue.h
@@ -77,6 +77,7 @@ public:
     static void createBufferQueue(sp<IGraphicBufferProducer>* outProducer,
             sp<IGraphicBufferConsumer>* outConsumer,
             const sp<IGraphicBufferAlloc>& allocator = NULL);
+
 private:
     BufferQueue(); // Create through createBufferQueue
 };
